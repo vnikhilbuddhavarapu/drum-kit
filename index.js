c62418c288f201c0455a3e2c30e3f5d1b2bf244a
vnikhilbuddhavarapu <127167499+vnikhilbuddhavarapu@users.noreply.github.com>
2024-03-25 16:30:31 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+//Detecting Button Presses
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+    });
+
+}
+
+//Detecting Key Presses
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+//Function to Play Sound
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio('/sounds/tom-1.mp3');
+            tom1.play();
+        break;
+
+        case "a":
+            var tom2 = new Audio('/sounds/tom-2.mp3');
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio('/sounds/tom-3.mp3');
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio('/sounds/tom-4.mp3');
+            tom4.play();
+        break;
+
+        case "j":
+            var snare = new Audio('/sounds/snare.mp3');
+            snare.play();
+        break;
+
+        case "k":
+            var crash = new Audio('/sounds/crash.mp3');
+            crash.play();
+        break;
+
+        case "l":
+            var kick = new Audio('/sounds/kick-bass.mp3');
+            kick.play();
+        break;
+
+        default: console.log(buttonInnerHTML);
+
+    }
+}
+
+
+function buttonAnimation(currentkey) {
+    var activeButton = document.querySelector("." + currentkey);
+    activeButton.classList.add("pressed");
+    
+    //Timeout function to remove the "Pressed" CSS Style
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+/*
+function handleClick() {
+    var audio = new Audio('/sounds/tom-1.mp3');
+    audio.play();
+}
+*/
